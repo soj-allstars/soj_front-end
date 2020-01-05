54eb0c797e0f128ad88534f25f4576d662f49b28
Hatsuroku <qianlanql@qq.com>
2020-01-05 18:56:23 +0800

    题目列表 第一页

--- src/components/ProblemSet.vue
+++ src/components/ProblemSet.vue
@@ -10,26 +10,31 @@
             <v-col cols='8' align="center">
                 <v-card class='pa-10'>
                     <div class="text-center">
-                        <v-row>
-                            <v-col cols='2' class="font-weight-black">id</v-col>
-                            <v-col class="font-weight-black">title</v-col>
+                        <v-row class="row_height">
+                            <v-col cols='3' class="font-weight-black pa-0 ma-0 row_height">
+                                id
+                            </v-col>
+                            <v-col class="font-weight-black pa-0 ma-0">
+                                title
+                            </v-col>
                         </v-row>
-                        <v-list>
-                            <v-list-item-group v-model="problem_cnt" color="primary">
-                                <v-list-item v-for="pbl in problems" 
-                                :key="pbl.id"
-                                class='pa-0'
-                                >
-                                    <v-col cols='2' class='pa-0 ma-0'>
-                                        <v-card outlined tile>aaa</v-card>
-                                    </v-col>
-                                    <v-col class='pa-0 ma-0'>bbb</v-col>
-                                </v-list-item>
-                            </v-list-item-group>
-                        </v-list>
+                        <v-row v-for="pbl in problems" :key="pbl.id"
+                         class="row_height"
+                         align="center"
+                         justify="center">
+                            <v-col cols='3' class="pa-0 ma-0 row_height">
+                                <v-btn text block outlined tile height="50px" class="text-none"
+                                link :to="{ name:'problem', params: { pid: pbl.id } }">{{pbl.id}}</v-btn>
+                            </v-col>
+                            <v-col class="pa-0 ma-0 row_height">
+                                <v-btn text block outlined tile height="50px" class="text-none"
+                                link :to="{ name:'problem', params: { pid: pbl.id } }">{{pbl.title}}</v-btn>
+                            </v-col>
+                        </v-row>
                         <v-pagination
                           v-model="page"
                           :length="6"
+                          class="mt-10"
                         ></v-pagination>
                     </div>
                 </v-card>
@@ -47,7 +52,6 @@
                 page: 1,
                 prev: '',
                 next: '',
-                table_headers: [ "id", "title" ],
                 problem_cnt: 2,
                 problems: [
                     {
@@ -60,10 +64,39 @@
                     }
                 ]
             }
+        },
+        beforeMount: function() {
+            var thisCom = this;
+            $.ajax({
+                //请求方式
+                type : "GET",
+                //请求地址
+                url : thisCom.serveUrl() + '/api/problems/',
+                //请求成功
+                success : function(result) {
+                    thisCom.problem_cnt = result.count;
+                    thisCom.prev = result.previous;
+                    thisCom.next = result.next;
+                    thisCom.problems = result.results;
+                },
+                //请求失败，包含具体的错误信息
+                error : function(e){
+                  console.log(e.status);
+                  console.log(e.responseText);
+                }
+            });
         }
     }
 </script>
 
 <style scoped>
-    
+    .row_height {
+        height: 50px;
+    }
+    .rela_pos {
+        position: relative;
+    }
+    .border {
+        border: 1px solid #aaa;
+    }
 </style>
