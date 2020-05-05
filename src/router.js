a99a807048a7a9c7a845e04ccf181e3682cd34bd
Swing <sgnxydx3211@163.com>
2020-05-05 14:34:40 +0800

    feat: enable Lazy Loading Routes

--- src/router.js
+++ src/router.js
@@ -1,22 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import mainPage from '@/components/mainPage'
-import problem from '@/components/problem'
-import problemSubmit from '@/components/problemSubmit'
-import problemSubmitRes from '@/components/problemSubmitRes'
-import problemSet from '@/components/problemSet'
-import login from '@/components/login'
-import addProblem from '@/components/addProblem'
-import problemSubmitResDetail from "@/components/problemSubmitResDetail";
-import contest from "@/components/contest";
-import contestSet from "@/components/contestSet";
-import contestProblem from "@/components/contestProblem";
-import contestProblemSubmit from "@/components/contestProblemSubmit";
-import liveResQuery from "@/components/liveResQuery";
-import normalSubmitRes from "@/components/normalSubmitRes";
+// import mainPage from '@/components/mainPage'
+// import problem from '@/components/problem'
+// import problemSubmit from '@/components/problemSubmit'
+// import problemSubmitRes from '@/components/problemSubmitRes'
+// import problemSet from '@/components/problemSet'
+// import login from '@/components/login'
+// import addProblem from '@/components/addProblem'
+// import problemSubmitResDetail from "@/components/problemSubmitResDetail";
+// import contest from "@/components/contest";
+// import contestSet from "@/components/contestSet";
+// import contestProblem from "@/components/contestProblem";
+// import contestProblemSubmit from "@/components/contestProblemSubmit";
+// import liveResQuery from "@/components/liveResQuery";
+// import normalSubmitRes from "@/components/normalSubmitRes";
 
 Vue.use(Router);
+
+const problem = () => import('@/components/problem');
+const problemSubmit = () => import('@/components/problemSubmit');
+const problemSubmitRes = () => import('@/components/problemSubmitRes');
+const problemSet = () => import('@/components/problemSet');
+const login = () => import('@/components/login');
+const addProblem = () => import('@/components/addProblem');
+const problemSubmitResDetail = () => import('@/components/problemSubmitResDetail');
+const contest = () => import("@/components/contest");
+const contestSet = () => import("@/components/contestSet");
+const contestProblem = () => import("@/components/contestProblem");
+const contestProblemSubmit = () => import("@/components/contestProblemSubmit");
+const liveResQuery = () => import("@/components/liveResQuery");
+const normalSubmitRes = () => import("@/components/normalSubmitRes");
 
 const routes = {
     routes: [
